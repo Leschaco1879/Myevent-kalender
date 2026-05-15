e5b8fc3deefe4ba05c9e4ed10c3f95b55f9177f7
Leschaco1879 <Kevin.Stedtnitz@leschaco.com>
2026-05-15 12:07:38 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://dukxmpgummncvzldpmmf.storage.supabase.co/storage/v1/s3";
+const supabaseKey = "sb_publishable_7KhkE7gYNMjfXEVH9kCNtQ_W-gVqxC4";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
